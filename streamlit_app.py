2b18da8b88de2094944d51f56fb67829fa49d01e
dipaktec <67356200+dipaktec@users.noreply.github.com>
2023-07-13 11:48:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,10 @@ import streamlit
 
 streamlit.title("My First streamlit diner")
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Coffee")
-streamlit.text("banana bread")
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
