5ba929b96e1454df878a062605ec6400f1cb2713
dipaktec <67356200+dipaktec@users.noreply.github.com>
2023-07-17 10:20:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit
+import pandas
+import requests
 import snowflake.connector
 
 
 streamlit.title("Mel's streamlit diner")
-
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
@@ -11,23 +12,15 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
-
-import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index ("Fruit")
-
 fruits_selected = streamlit.multiselect("Pick fruits", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 streamlit.dataframe (fruits_to_show)
 
 streamlit.header ('Fruityvice food advice')
-
 fruit_choice = streamlit.text_input('what fruit would you like information about ' , 'Kiwi')
 streamlit.write('user entered '+  fruit_choice)
-
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # write your own comment -this normalizes the json to better readble format 
@@ -49,3 +42,9 @@ streamlit.dataframe(my_data_row)
 
 fruit_choice_add = streamlit.text_input('what fruit would you like to add ' , 'jackfruit')
 streamlit.write('Thanks for adding '+  fruit_choice_add)
+
+my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ("from streamlit"))
+
+
+
+
